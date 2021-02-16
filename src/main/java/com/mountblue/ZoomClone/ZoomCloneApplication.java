79b887934eb2b4ba7d8faebee88ac43595da6f2b
akashmjain <business@akashmjain.com>
2021-02-16 13:33:00 +0530

    Initial Commit

---
+++ src/main/java/com/mountblue/ZoomClone/ZoomCloneApplication.java
@@ -0,0 +1,13 @@
+package com.mountblue.ZoomClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mountblue/ZoomClone/ZoomCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountblue.ZoomClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
